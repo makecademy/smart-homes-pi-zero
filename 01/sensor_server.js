70878dae11c8f7d3ef96bc8e293f5069fb7bbaa3
marcoschwartz <marcolivier.schwartz@gmail.com>
2016-05-22 11:22:51 +0200

    initial commit

---
+++ 01/sensor_server.js
@@ -0,0 +1,39 @@
+// Sensor
+var sensorLib = require('node-dht-sensor');
+
+// Express app
+var express = require('express');
+var app = express();
+
+// Main route
+app.get('/', function (req, res) {
+
+  var readout = sensor.read();
+  res.send('Temperature: ' + readout.temperature.toFixed(2));
+  
+});
+
+// Start server
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+
+
+// Sensor
+var sensor = {
+    initialize: function () {
+        return sensorLib.initialize(11, 4);
+    },
+    read: function () {
+
+        // Read
+        var readout = sensorLib.read();
+        return readout;   
+    }
+};
+
+if (sensor.initialize()) {
+    sensor.read();
+} else {
+    console.warn('Failed to initialize sensor');
+}
