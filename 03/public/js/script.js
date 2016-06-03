74ab60abf87fb338ac515e9e6ebab38b337852d5
marcoschwartz <marcolivier.schwartz@gmail.com>
2016-06-03 11:42:16 +0200

    update

--- 03/public/js/script.js
+++ 03/public/js/script.js
@@ -3,12 +3,14 @@ $( document ).ready(function() {
   // Get data
   $.get('/get', function(data) {
 
+    console.log(data);
     $('#thermostat').html(data.targetTemperature);
 
   });
 
   $.get('/temperature', function(data) {
 
+    console.log(data);
     $('#temperature').html(data.temperature);
 
   });
