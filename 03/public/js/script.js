c3bd1ef6b910461db5898b8b5cfb282b4a491157
marcoschwartz <marcolivier.schwartz@gmail.com>
2016-06-03 11:32:52 +0200

    update

--- 03/public/js/script.js
+++ 03/public/js/script.js
@@ -1,59 +1,17 @@
 $( document ).ready(function() {
 
-  // Format data
-  $.get('/data', function(measurements) {
+  // Get data
+  $.get('/get', function(data) {
 
-    var dates = [];
-    var temperature = [];
-    var humidity = [];
+    $('#thermostat').val(data);
 
-    console.log(measurements);
+  });
 
-    for (i = 0; i < measurements.length; i++) {
+  $.get('/temperature', function(data) {
 
-        dates.push(measurements[i].date);
-        temperature.push(parseFloat(measurements[i].temperature));
-        humidity.push(parseFloat(measurements[i].humidity));
+    $('#temperature').val(data);
 
-    }
+  });
 
-    // Plot
-    $('#container').highcharts({
-        title: {
-            text: 'Temperature & Humidity Data',
-            x: -20 //center
-        },
-        xAxis: {
-            categories: dates
-        },
-        yAxis: {
-            title: {
-                text: 'Temperature (°C)'
-            },
-            plotLines: [{
-                value: 0,
-                width: 1,
-                color: '#808080'
-            }]
-        },
-        tooltip: {
-            valueSuffix: '°C'
-        },
-        legend: {
-            layout: 'vertical',
-            align: 'right',
-            verticalAlign: 'middle',
-            borderWidth: 0
-        },
-        series: [{
-            name: 'Temperature',
-            data: temperature
-        },
-        {
-            name: 'Humidity',
-            data: humidity
-        }]
-    });
 
-    });
 });
