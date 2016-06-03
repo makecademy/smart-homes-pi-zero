e9fe0d63e40463686192261acf35e9ff10535cf5
marcoschwartz <marcolivier.schwartz@gmail.com>
2016-06-03 10:09:38 +0200

    update

---
+++ 03/thermostat_server.js
@@ -0,0 +1,59 @@
+// Modules
+var sensorLib = require('node-dht-sensor');
+var express = require('express');
+
+// Express app
+var app = express();
+var piREST = require('pi-arest')(app);
+
+// aREST configuration
+piREST.set_id('34f5eQ');
+piREST.set_name('my_rpi_zero');
+
+// Thermostat settings
+var targetTemperature = 25;
+var threshold = 1;
+
+// Routes
+app.get('/get', function (req, res) {
+
+  answer = {
+    targetTemperature: targetTemperature
+  };
+  res.json(answer);
+
+});
+
+app.get('/temperature', function (req, res) {
+
+  answer = {
+    temperature: sensor.read().temperature.toFixed(2)
+  };
+  res.json(answer);
+
+});
+
+// Start server
+app.listen(3000, function () {
+  console.log('Raspberry Pi Zero thermostat started!');
+});
+
+
+// Sensor
+var sensor = {
+    initialize: function () {
+        return sensorLib.initialize(11, 4);
+    },
+    read: function () {
+
+        // Read
+        var readout = sensorLib.read();
+        return readout;
+    }
+};
+
+if (sensor.initialize()) {
+    sensor.read();
+} else {
+    console.warn('Failed to initialize sensor');
+}
