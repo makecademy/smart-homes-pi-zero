84cdd34c3c1b5d2ba9e21b33c7094100e3da8b65
marcoschwartz <marcolivier.schwartz@gmail.com>
2016-07-28 10:00:36 +0200

    added sms alerts

---
+++ 06/motion_sms_alerts/sms_alerts.js
@@ -0,0 +1,59 @@
+// Required modules
+var request = require('request');
+var gpio = require('rpi-gpio');
+
+// IFTTT data
+var key = "dPMHywdahaSxQZlCaoqnzHxcQ8vNYsTlk";
+
+// Motion sensor GPIO
+var motionSensorPin = 18;
+
+// Counter between two alerts
+var interval = 60 * 1000; // 1 minute
+var counter = new Date();
+
+// Setup gpio library
+gpio.setMode('MODE_BCM');
+
+// Check status every second
+setInterval(function() {
+
+  // Check counter so we don't trigger alarms all the time
+  var currentTime = (new Date()).getTime();
+  var counterTime = counter.getTime();
+  if (currentTime - counterTime > interval) {
+
+    // Check sensor
+    gpio.setup(motionSensorPin, gpio.DIR_IN, checkSensor);
+
+  }
+
+}, 1000);
+
+// Check motion sensor
+function checkSensor() {
+  gpio.read(motionSensorPin, function(err, value) {
+
+      // If motion is detected, send event to IFTTT
+      if (value == true) {
+
+        // Restart Counter
+        counter = new Date();
+
+        // Send event
+        alertIFTTT();
+      }
+  });
+}
+
+// Make request
+function alertIFTTT() {
+
+  // Send alert to IFTTT
+  var url = 'https://maker.ifttt.com/trigger/{event}/with/key/' + key;
+  request(url, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      console.log("Alert sent to IFTTT");
+    }
+  });
+}
